fb893eb394862dcf1afca41f279f07b6e121aefb
ATQQ <2604395430@qq.com>
2021-04-20 20:40:25 +0800

    feat: 小改样式,加入js

---
+++ 仿微信登录窗口/js/index.js
@@ -0,0 +1,17 @@
+const nickname = 'Zoe'
+const wechatAccount = 'wxid_p1ty0my7vide22'
+const navs = [
+    { text: '支付', icon: 'icon-zhifu' },
+    { text: '收藏', icon: 'icon-shoucang' },
+    // ...其余自己补充
+]
+
+/**
+ * 补全这个方法,通过这个方法渲染index.html中的内容
+ */
+function render(){
+    // 使用domAPI操作
+    // ..code
+}
+
+render()
